8053d9b94a6ddb08c0412fab3fbacbc2826eb80a
Agrim Saxena <agrimsaxena9@gmail.com>
2026-07-23 16:58:56 +0530

    s

--- backend/index.js
+++ backend/index.js
@@ -25,8 +25,8 @@ app.get('/', (req, res) => {
 
 app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
 });
 
 server.listen(PORT, () => {
